c8f7252b4405f4dfaf9042696404a0d807ec52bb
mohammednazim007 <mohammednazim3629@gmail.com>
2026-02-17 19:36:27 +0600

    fix: add missing comma in BillingController export
    The missing comma caused a syntax error when adding new methods to the controller. Adding the trailing comma ensures consistent formatting and prevents future syntax errors during extension.

--- dist/app/modules/analytics/analytics.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import { Router } from "express";
-// import { AnalyticsController } from "./analytics.controller";
-// const router = Router();
-// // router.get("/", AnalyticsController.getAnalytics);
-// router.get("/dashboard", AnalyticsController.getDashboardAnalytics);
-// export const AnalyticsRoute = router;
--- dist/app/modules/sent-message/sent-message.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MESSAGE_FILTER_FIELDS = exports.MESSAGE_SEARCH_FIELDS = void 0;
-exports.MESSAGE_SEARCH_FIELDS = ["subject", "recipientEmail"];
-exports.MESSAGE_FILTER_FIELDS = ["status"];
